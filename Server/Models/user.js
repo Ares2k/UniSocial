dff19be0474a9357f32fde0a46e2ad9edd969b50
Timur Sultanov <Timursultanov00@gmail.com>
2022-01-29 19:26:03 +0000

    Modify user schema to include course information

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  email:     { type: String, required: true, unique: true },
-  password:  { type: String, required: true },
-  firstname: { type: String, required: true },
-  surname:   { type: String, required: true },
-  course:    { type: String, required: false },
-  year:      { type: Number, required: false },
-  bio:       { type: String, required: true },
-  hobbies:   { type: Array,  required: false, default: []}
+  email     : { type: String, required: true, unique: true },
+  password  : { type: String, required: true },
+  firstname : { type: String, required: true },
+  surname   : { type: String, required: true },
+  course    : {
+    code: String,
+    name: String,
+    year: Number
+  },
+  bio       : { type: String },
+  hobbies   : { type: Array, default: [] }
 }, {collection: 'users'});
 
 const model = mongoose.model('UserSchema', UserSchema);
