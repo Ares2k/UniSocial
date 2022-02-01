fdd7227f22826b960201a5cbfd422b2a407db7f7
Timur Sultanov <Timursultanov00@gmail.com>
2022-02-01 20:59:33 +0000

    Add username back into schema for specific profile search under a username

--- Server/Models/user.js
+++ Server/Models/user.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
+  username  : { type: String, required: true, unique: true },
   email     : { type: String, required: true, unique: true },
-  password  : { type: String, required: true },
   firstname : { type: String, required: true },
   surname   : { type: String, required: true },
+  password  : { type: String, required: true },
   course    : {
     code: String,
     name: String,
