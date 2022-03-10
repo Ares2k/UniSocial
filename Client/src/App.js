677cc82520a86e2ee83bda5d3181063ee1de3af4
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-10 00:14:11 +0000

    Added endpoint for a user searchable by username

--- Client/src/App.js
+++ Client/src/App.js
@@ -3,19 +3,23 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Views/Register/Register';
 import Login from './Views/Login/Login';
-import UserProfile from './Views/UserProfile';
-import MutualUsers from './Views/MutualUsers';
+import UserProfile from './User/UserProfile';
+import MutualUsers from './Views/MutualUsers/MutualUsers';
+import Navbar from './Components/Navbar/Navbar';
+import DisplayUser from './Views/MutualUsers/DisplayUser';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
+          <Navbar />
           <Routes>
             <Route path='/register' element={<Register />}/>
             <Route path='/login' element={<Login />}/>
             <Route path='/profile/edit' element={<UserProfile />}/>
             <Route path='/users/mutual' element={<MutualUsers />}/>
+            <Route path='/users/:id' element={<DisplayUser />}/>
           </Routes>
         </div>
       </Router>
