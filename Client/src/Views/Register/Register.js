4f2663e4bcc82ca247ae22dbd982268d0a835a57
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-10 00:25:58 +0000

    Added and styled registration page

--- Client/src/Views/Register/Register.js
+++ Client/src/Views/Register/Register.js
@@ -1,12 +1,14 @@
-import Button from "../../Components/Button";
-import './register.css';
+import Button from "../../Components/Button/Button";
+// import './register.css';
 import { FaUserAlt } from 'react-icons/fa';
 import { FaUserCircle } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 import { FaLock } from 'react-icons/fa';
 import phoneImg from '../../Assets/Images/unisocial-phone.svg';
+import register from './register.module.css';
+import style from '../../Components/Button/button.module.css';
 
-const register = async (e) => {
+const handleSubmit = async (e) => {
   e.preventDefault();
 
   const username = document.getElementById('username').value
@@ -32,49 +34,71 @@ const register = async (e) => {
   });
 
   const content = await response.json();
-
   console.log(content);
 }
 
 const Register = () => {
 
   return (
-    <div className="container">
-      <div className="registration-box">
-        <img src={phoneImg} alt=""/>
-        <div className="right">
-          <h1>Create your account</h1>
-          <p>It's quick and easy.</p>
-            <input className="field" id='username'        placeholder='Username'         type='text'     autoComplete='off' />
-            <input className="field" id='firstname'       placeholder='First Name'       type='text'     autoComplete='off' />
-            <input className="field" id='surname'         placeholder='Surname'          type='text'     autoComplete='off' />
-            <input className="field" id='email'           placeholder='Email Address'    type='email'    autoComplete='off' />
-            <input className="field" id='password'        placeholder='Password'         type='password' autoComplete='off' />
-            <input className="field" id='confirmPassword' placeholder='Confirm Password' type='password' autoComplete='off' />
-            <Button label='Register' onClick={register}/>
+    <div className={register.container}>
+      <div className={register.registrationBox}>
+        <img className={register.img} src={phoneImg} alt=""/>
+        <div className={register.right}>
+          <h1 className={register.h1}>Create your account</h1>
+          <p className={register.p}>It's quick and easy.</p>
+
+            <input
+              className={register.field}
+              id='username'
+              placeholder='Username'
+              type='text'
+              autoComplete='off'
+            />
+
+            <input 
+              className={register.field}
+              id='firstname'
+              placeholder='First Name'
+              type='text'
+              autoComplete='off'
+            />
+
+            <input 
+              className={register.field}
+              id='surname'
+              placeholder='Surname'
+              type='text'
+              autoComplete='off'
+            />
+
+            <input 
+              className={register.field}
+              id='email'
+              placeholder='Email Address'
+              type='email'
+              autoComplete='off'
+            />
+
+            <input 
+              className={register.field}
+              id='password'
+              placeholder='Password'
+              type='password'
+              autoComplete='off'
+            />
+
+            <input 
+              className={register.field}
+              id='confirmPassword'
+              placeholder='Confirm Password'
+              type='password'
+              autoComplete='off'
+            />
+            
+            <Button label='Register' onClick={handleSubmit} className={style.button}/>
         </div>
       </div>
     </div>
-
-
-
-    // <form className="form" onSubmit={handleSubmit}>
-    //   <div id="icons">
-    //     <FaUserCircle className="user-icon" size={'1.5em'}/>
-    //     <FaUserAlt className="user-icon" size={'1.5em'}/>
-    //     <FaUserAlt className="user-icon" size={'1.5em'}/>
-    //     <MdEmail className="user-icon" size={'1.5em'}/>
-    //     <FaLock className="user-icon" size={'1.5em'}/>
-    //     <FaLock className="user-icon" size={'1.5em'}/>
-    //   </div>
-    //   <input id='username'        placeholder='Username'         type='text'     autoComplete='off' />
-    //   <input id='firstname'       placeholder='First Name'       type='text'     autoComplete='off' />
-    //   <input id='surname'         placeholder='Surname'          type='text'     autoComplete='off' />
-    //   <input id='email'           placeholder='Email Address'    type='email'    autoComplete='off' />
-    //   <input id='password'        placeholder='Password'         type='password' autoComplete='off' />
-    //   <input id='confirmPassword' placeholder='Confirm Password' type='password' autoComplete='off' />
-    //   <Button label='Register' onClick={register}/>
-    // </form>
   );
 }
  
