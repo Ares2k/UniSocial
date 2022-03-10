7105b6397bbc468c013a4d3c7876f4610e8bb31d
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-10 00:33:01 +0000

    Minor renaming

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -6,12 +6,13 @@ const checkAuth = require('../Middleware/authentication');
 router.post('/register', userController.register);
 router.post('/login'   , userController.login);
 router.post('/token'   , userController.generateNewToken);
+router.get('/hobbies'  , userController.getHobbies);
 router.post('/logout'          , checkAuth, userController.logout);
 router.post('/change-password' , checkAuth, userController.changePassword);
-router.get('/profile'          , checkAuth, userController.userProfile);
-router.put('/profile'          , checkAuth, userController.editUserProfile);
+router.get('/profile'          , checkAuth, userController.getProfile);
+router.put('/profile'          , checkAuth, userController.updateProfile);
 router.get('/mutual/'          , checkAuth, userController.mutualUsers);
-router.get('/mutual/:id'       , checkAuth, userController.mutualUser)
+router.get('/mutual/:id'       , checkAuth, userController.mutualUser);
 //TODO: Add a delete profile endpoint
 
 module.exports = router;
