8b487f588e68415e0ba784b30b1bbcc87ea7d389
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-12 02:04:12 +0000

    Added hobby select box for multiple hobby selection in the popup modal

--- Client/src/Components/Popup/Popup.js
+++ Client/src/Components/Popup/Popup.js
@@ -5,16 +5,12 @@ import { useEffect, useState } from 'react';
 import Select from 'react-select';
 
 const Popup = (props) => {
-  // const [input, setInput] = useState('');
   const [hobbies, setHobbies] = useState([]);
   const [selectedHobbies, setSelectedHobbies] = useState([]);
-  // let profileHobbies = [];
-  // const [userHobbies, setUserHobbies] = useState([]);
-  // let hobby = [];
 
   useEffect(() => {
     const fetchHobbies = async () => {
-      let hobbyList = await fetch('http://localhost:5000/api/hobbies', {
+      let hobbyList = await fetch('http://192.168.0.74:5000/api/hobbies', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json'
@@ -23,49 +19,14 @@ const Popup = (props) => {
       
       const { hobbies } = await hobbyList.json();
       setHobbies(hobbies);
-
-      // const userHobbies = props.profile.hobbies;
-      // for(let i=0; i<userHobbies.length; i++) {
-      //   profileHobbies.push({
-      //     value: i,
-      //     label: userHobbies[i]
-      //   })
-      // }
-      // console.log(profileHobbies);
-      // setSelectedHobbies(profileHobbies);
     }
     fetchHobbies();
   }, []);
 
   const handleChange = (e) => {
-    // console.log(props.profile.hobbies);
     const selected = Array.isArray(e) ? e.map(hobby => hobby.label) : [];
     setSelectedHobbies(selected);
-    // props.setHobbies(selected)
   }
-
-  // const handleCheckBox = (e) => {
-  //   if(e.target.checked) {
-  //     setUserHobbies([...userHobbies, e.target.value]);
-  //   } else {
-  //     // console.log(e.target.value + ' needs to be removed');
-  //     const checkBoxArr = userHobbies.filter((hobby) => {
-  //       return (hobby !== e.target.value);
-  //     });
-
-  //     setUserHobbies(checkBoxArr);
-  //   }
-
-  //   console.log(userHobbies);
-  // }
-
-  // if(input.length > 0) {
-  //   hobby = hobbies.filter((hobby) => {
-  //     return hobby.hobby.toLowerCase().includes(input.toLowerCase())
-  //   });
-  // } else {
-  //   hobby = hobbies;
-  // }
 
   return (props.trigger) ? (
     <div className={style.popup}>
@@ -81,14 +42,6 @@ const Popup = (props) => {
             })
           }}
         />
-
-        {/* <input
-          id={style.hobbySearch}
-          type='text'
-          placeholder='Start typing a hobby...'
-          onChange={ handleChange }
-          value={ input }
-        /> */}
         
         <div className={style.hobbies}>
           <Select
@@ -96,12 +49,6 @@ const Popup = (props) => {
             options={hobbies}
             onChange={handleChange}
             className={style.select}/>
-          {/* {hobby.map((hobby, index) => (
-            <div key={index} className={style.hobby}>
-              <input type="checkbox" onChange={handleCheckBox} value={ hobby.hobby }/>
-              { hobby.hobby }
-            </div>
-          ))} */}
         </div>
       </div>
     </div>
