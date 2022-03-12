585fbd0adbca540ae4b209253b1da300ec1c14df
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-12 01:50:49 +0000

    Removed button styling since individual styling can be applied, removed redundant code

--- Client/src/Components/Button/Button.js
+++ Client/src/Components/Button/Button.js
@@ -1,30 +1,11 @@
-import React, { Component } from 'react';
-// import button from './button.module.css';
-
 const Button = (props) => {
   return (
     <button
       className={props.className}
       onClick={props.onClick}>
-
-    {props.label}
+      {props.label}
     </button>  
   );
 }
  
 export default Button;
-
-// class Button extends Component {
-//   render() {
-//     return (
-//       <button
-//         className={this.props.className}
-//         onClick={this.props.onClick}
-//       >
-//         {this.props.label}
-//       </button>
-//     );
-//   }
-// }
-
-// export default Button;
