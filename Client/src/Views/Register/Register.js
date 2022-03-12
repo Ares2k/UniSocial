9b91ad3741472795ba443acf5ac645021c34b163
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-12 02:05:05 +0000

    Added a login button in the case that a user already has an account

--- Client/src/Views/Register/Register.js
+++ Client/src/Views/Register/Register.js
@@ -5,50 +5,54 @@ import { FaUserCircle } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 import { FaLock } from 'react-icons/fa';
 import phoneImg from '../../Assets/Images/unisocial-phone.svg';
-import register from './register.module.css';
-import style from '../../Components/Button/button.module.css';
-
-const handleSubmit = async (e) => {
-  e.preventDefault();
-
-  const username = document.getElementById('username').value
-  const email = document.getElementById('email').value
-  const password = document.getElementById('password').value
-  const firstname = document.getElementById('firstname').value
-  const surname = document.getElementById('surname').value
-  const confirmPassword = document.getElementById('confirmPassword').value;
-
-  const response = await fetch('http://localhost:5000/api/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      "username": username,
-      "email": email,
-      "password": password,
-      "confirmPass": confirmPassword,
-      "firstname": firstname,
-      "surname": surname
-    })
-  });
-
-  const content = await response.json();
-  console.log(content);
-}
+import phoneImgOrange from '../../Assets/Images/unisocial-phone-orange.svg';
+import style from './register.module.css';
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value
+    const email = document.getElementById('email').value
+    const password = document.getElementById('password').value
+    const firstname = document.getElementById('firstname').value
+    const surname = document.getElementById('surname').value
+    const confirmPassword = document.getElementById('confirmPassword').value;
+
+    const response = await fetch('http://192.168.0.74:5000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        "username": username,
+        "email": email,
+        "password": password,
+        "confirmPass": confirmPassword,
+        "firstname": firstname,
+        "surname": surname
+      })
+    });
+
+    const content = await response.json();
+    console.log(content);
+
+    navigate('/users/mutual');
+  }
 
   return (
-    <div className={register.container}>
-      <div className={register.registrationBox}>
-        <img className={register.img} src={phoneImg} alt=""/>
-        <div className={register.right}>
-          <h1 className={register.h1}>Create your account</h1>
-          <p className={register.p}>It's quick and easy.</p>
+    <div className={style.container}>
+      <div className={style.registrationBox}>
+        <img className={style.img} src={phoneImgOrange} alt=""/>
+        <div className={style.right}>
+          <h1 className={style.h1}>Create your account</h1>
+          <p className={style.p}>It's quick and easy.</p>
 
             <input
-              className={register.field}
+              className={style.field}
               id='username'
               placeholder='Username'
               type='text'
@@ -56,7 +60,7 @@ const Register = () => {
             />
 
             <input 
-              className={register.field}
+              className={style.field}
               id='firstname'
               placeholder='First Name'
               type='text'
@@ -64,7 +68,7 @@ const Register = () => {
             />
 
             <input 
-              className={register.field}
+              className={style.field}
               id='surname'
               placeholder='Surname'
               type='text'
@@ -72,7 +76,7 @@ const Register = () => {
             />
 
             <input 
-              className={register.field}
+              className={style.field}
               id='email'
               placeholder='Email Address'
               type='email'
@@ -80,7 +84,7 @@ const Register = () => {
             />
 
             <input 
-              className={register.field}
+              className={style.field}
               id='password'
               placeholder='Password'
               type='password'
@@ -88,14 +92,17 @@ const Register = () => {
             />
 
             <input 
-              className={register.field}
+              className={style.field}
               id='confirmPassword'
               placeholder='Confirm Password'
               type='password'
               autoComplete='off'
             />
-            
-            <Button label='Register' onClick={handleSubmit} className={style.button}/>
+          
+          <div className={style.buttonHolder}>
+            <Button label='Sign Up »' onClick={handleSubmit} className={style.button}/>
+            <Button label='Log in Instead?' onClick={() => navigate('/login')} className={style.button} />
+          </div>
         </div>
       </div>
     </div>
