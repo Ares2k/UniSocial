1c12ddf07fd7a5e5f2d8833e1ee5e8cd1c255213
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-14 00:15:20 +0000

    Minor code changes

--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -7,14 +7,17 @@ const Navbar = () => {
   return (
     <div className={navbar.navbar}>
       <Link to="/users/mutual" className={navbar.brandTitle}>UniSocial</Link>
+
       <div className={navbar.icons}>
         <Link className={navbar.iconLinks} to='#'>
           <BsFillChatDotsFill style={{width: '25px', height: '25px'}}/>
         </Link>
+
         <Link className={navbar.iconLinks} to='/profile/edit'>
-          <FaUserAlt style={{width: '25px', height: '25px'}}/>
+          <FaUserAlt size={"25px"}/>
         </Link>
       </div>
+
       <div className={navbar.links}>
           <Link to="#">Chat</Link>
           <Link to="/profile/edit">Profile</Link>
