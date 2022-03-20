12337507a0b6c77dd69cb369d4cdeb7de034fb4f
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-20 22:51:41 +0000

    Added link/icon for meeting mutual students

--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -2,23 +2,29 @@ import navbar from './navbar.module.css';
 import { Link } from 'react-router-dom';
 import { BsFillChatDotsFill } from 'react-icons/bs';
 import { FaUserAlt } from 'react-icons/fa';
+import { ImUsers } from 'react-icons/im';
 
 const Navbar = () => {
   return (
     <div className={navbar.navbar}>
-      <Link to="/users/mutual" className={navbar.brandTitle}>UniSocial</Link>
+      <Link to="/home" className={navbar.brandTitle}>UniSocial</Link>
 
       <div className={navbar.icons}>
+        <Link className={navbar.iconLinks} to='/mutual'>
+          <ImUsers style={{width: '30px', height: '30px'}}/>
+        </Link>
+
         <Link className={navbar.iconLinks} to='#'>
-          <BsFillChatDotsFill style={{width: '25px', height: '25px'}}/>
+          <BsFillChatDotsFill style={{width: '30px', height: '30px'}}/>
         </Link>
 
         <Link className={navbar.iconLinks} to='/profile/edit'>
-          <FaUserAlt size={"25px"}/>
+          <FaUserAlt size={"30px"}/>
         </Link>
       </div>
 
       <div className={navbar.links}>
+          <Link to="/mutual">Meet Students</Link>
           <Link to="#">Chat</Link>
           <Link to="/profile/edit">Profile</Link>
       </div>
