614d2256075c0c37a8289f834a38da0a03c1b2e2
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-20 01:27:03 +0000

    Modified route to include fetching/posting profile picture

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../Controllers/userController');
 const checkAuth = require('../Middleware/authentication');
+const multer = require('multer');
+
+const upload = multer({ dest: 'uploads/' });
 
 router.post('/register', userController.register);
 router.post('/login'   , userController.login);
@@ -13,6 +16,9 @@ router.get('/profile'          , checkAuth, userController.getProfile);
 router.put('/profile'          , checkAuth, userController.updateProfile);
 router.get('/mutual/'          , checkAuth, userController.mutualUsers);
 router.get('/mutual/:id'       , checkAuth, userController.mutualUser);
+router.put('/images'           , checkAuth, upload.single('image'), userController.uploadImage);
+router.get('/images/:key'      , userController.getImage);
+router.get('/populate-hobbies', userController.populateHobbies); // Populate hobbies in database from txt file
 //TODO: Add a delete profile endpoint
 
 module.exports = router;
