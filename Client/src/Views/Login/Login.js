6c7357c522cdd52552b59c3f9ae71f6dc24e78d6
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-20 22:52:32 +0000

    Customized login screen with error checking

--- Client/src/Views/Login/Login.js
+++ Client/src/Views/Login/Login.js
@@ -1,61 +1,106 @@
 import Button from "../../Components/Button/Button";
-import { Navigate, useLocation, useNavigate  } from "react-router-dom";
+import { Link, Navigate, useLocation, useNavigate  } from "react-router-dom";
 import { useState } from "react";
+import style from './login.module.css';
+import { validateLogin } from '../../Helpers/validateInput';
 
 const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const [token, setToken] = useState(localStorage.getItem('token'));
+  const [error, setError] = useState({
+    usernameErr: '',
+    passwordErr: ''
+  })
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async () => {
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
+    let isMounted = true;
   
-    let response = await fetch('http://192.168.0.74:5000/api/login', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        username,
-        password
-      })
-    });
-  
-    response = await response.json();
+    const userInput = validateLogin({username, password});
     
-    if(response.status === 401) {
-      console.log(response);
-      /*
-       * Clear JWT token upon unsuccessful login to 
-       * prevent token tampering/fabrication */
-      localStorage.removeItem('token');
-      setToken(null);
-    } else {
-      localStorage.setItem('token', response.token);
-      setToken(true);
-
-      /* 
-       * If a protected route was attempted to be accessed before
-       * prompting login, visit that route otherwise redirect
-       * to mutual users */
-      if(location?.state?.from) {
-        navigate(location.state.from);
+    if(!userInput.usernameErr && !userInput.passwordErr) {
+      let response = await fetch('http://192.168.0.74:5000/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+  
+      response = await response.json();
+      
+      if(response.status !== 200) {
+        localStorage.removeItem('token');
+        setError({
+          usernameErr: response.error
+        })
+        return isMounted ? setToken(null) : null;
       } else {
-        navigate('/users/mutual');
+        localStorage.setItem('token', response.token);
+
+        if (isMounted)
+          setToken(true);
+
+        if(location?.state?.from) {
+          navigate(location.state.from);
+        } else {
+          navigate('/mutual');
+        }
       }
+    } else {
+      setError({
+        usernameErr: userInput.usernameErr,
+        passwordErr: userInput.passwordErr
+      })
     }
   }
 
   return !token ? (
-    <div className="container">
-      <input id='username' placeholder='Username' type='text' autoComplete='off' />
-      <input id='password' placeholder='Password' type='password' autoComplete='off' />
-      <Button label='Sign In' onClick={handleSubmit}/>
+    <div className={style.form}>
+      <h1>UniSocial</h1>
+      <div className={style.loginForm}>
+        <p className={style.welcomeBack}>
+          Welcome back!
+        </p>
+
+        { error.usernameErr ? <div className={style.errMsg}>{error.usernameErr}</div> : <div>&nbsp;</div> }
+        <div className={style.inputWrapper}>
+          <div className={style.centeredInput}>
+            <p>Username</p>
+            <input
+              id='username'
+              type='text'
+              autoComplete='off'/>
+          </div>
+        </div>
+        
+        { error.passwordErr ? <div className={style.errMsg}>{error.passwordErr}</div> : <div>&nbsp;</div> }
+        <div className={style.inputWrapper}>
+          <div className={style.centeredInput}>
+            <p>Password</p>
+            <input id='password' type='password' autoComplete='off' />
+          </div>
+        </div>
+
+        <Button
+          label='Sign In »'
+          onClick={handleSubmit}
+          className={style.button}
+        />
+      </div>
+
+      <p className={style.links}>
+        Don't have an account?
+        <Link to="/register" style={{color: "#df691a", textDecoration: "none", fontWeight: "bold"}}>Register here.</Link>
+      </p>
     </div>
-  ) : <Navigate to="/users/mutual"/>;
+  ) : <Navigate to="/mutual" state={{ from: location }}/>;
 }
  
 export default Login;
