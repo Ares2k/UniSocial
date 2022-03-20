5de3389d8dddd12701390fd9ea308cb652bbedc0
Timur Sultanov <Timursultanov00@gmail.com>
2022-03-20 01:32:36 +0000

    Added extra error checking for when bearer token is missing

--- Server/Middleware/authentication.js
+++ Server/Middleware/authentication.js
@@ -1,8 +1,15 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
+  if(!req.headers.authorization) {
+    return res.json({ status: 401, error: 'JWT token missing' });
+  }
+  
   const token = req.headers.authorization.split(' ')[1];
-  if(!token) return res.json({ status: 401, error: 'Invalid JWT token' });
+
+  if(!token) {
+    return res.json({ status: 401, error: 'Invalid JWT token' });
+  }
 
   try {
     const user = jwt.verify(token, process.env.TOKEN_SECRET);
