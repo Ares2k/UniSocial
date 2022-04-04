dcd5ec2ce4320ba24c6ac99044772f9dea3b7880
Timur Sultanov <Timursultanov00@gmail.com>
2022-04-04 04:35:10 +0100

    Minor change

--- Server/Models/hobbies.js
+++ Server/Models/hobbies.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const HobbiesSchema = new mongoose.Schema({
   value: { type: String, required: true, unique: true },
-  label:  { type: String, required: true, unique: true }
+  label: { type: String, required: true, unique: true }
 }, { collection: 'hobbies' });
 
 const model = mongoose.model('HobbiesSchema', HobbiesSchema);
