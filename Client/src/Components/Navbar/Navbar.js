cd45d0787f038bd00c241a36ebd034618be1c004
Timur Sultanov <Timursultanov00@gmail.com>
2022-04-04 04:22:15 +0100

    Added react context for signout option

--- Client/src/Components/Navbar/Navbar.js
+++ Client/src/Components/Navbar/Navbar.js
@@ -3,30 +3,48 @@ import { Link } from 'react-router-dom';
 import { BsFillChatDotsFill } from 'react-icons/bs';
 import { FaUserAlt } from 'react-icons/fa';
 import { ImUsers } from 'react-icons/im';
+import { useContext } from 'react';
+import { NavbarContext } from '../../App';
+import { MdLogout } from 'react-icons/md';
 
 const Navbar = () => {
+  const { navVal } = useContext(NavbarContext);
+
+  const logout = () => {
+    localStorage.removeItem('token');
+  }
+
   return (
     <div className={navbar.navbar}>
       <Link to="/home" className={navbar.brandTitle}>UniSocial</Link>
 
       <div className={navbar.icons}>
         <Link className={navbar.iconLinks} to='/mutual'>
-          <ImUsers style={{width: '30px', height: '30px'}}/>
+          <ImUsers size={"30px"}/>
         </Link>
 
         <Link className={navbar.iconLinks} to='#'>
-          <BsFillChatDotsFill style={{width: '30px', height: '30px'}}/>
+          <BsFillChatDotsFill size={"30px"}/>
         </Link>
 
-        <Link className={navbar.iconLinks} to='/profile/edit'>
-          <FaUserAlt size={"30px"}/>
+        <Link
+          className={navbar.iconLinks}
+          to={!navVal && '/profile/edit'}
+          onClick={navVal && logout}
+        >
+          {navVal ? <MdLogout size={"35px"}/> : <FaUserAlt size={"30px"}/>}
         </Link>
       </div>
 
       <div className={navbar.links}>
-          <Link to="/mutual">Meet Students</Link>
-          <Link to="#">Chat</Link>
-          <Link to="/profile/edit">Profile</Link>
+        <Link to="/mutual">Meet Students</Link>
+        <Link to="#">Chat</Link>
+        <Link
+          to={!navVal && "/profile/edit"}
+          onClick={navVal && logout}
+        >
+          {navVal || "Profile"}
+        </Link>
       </div>
     </div>
   );
