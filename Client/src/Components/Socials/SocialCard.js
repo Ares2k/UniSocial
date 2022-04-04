4e8240a9397ba76889d1ab5600e60506e70f906b
Timur Sultanov <Timursultanov00@gmail.com>
2022-04-04 04:23:50 +0100

    Added ability to remove social links

--- Client/src/Components/Socials/SocialCard.js
+++ Client/src/Components/Socials/SocialCard.js
@@ -4,6 +4,7 @@ import linkedInImg from '../../Assets/Images/linkedin.svg';
 import facebookImg from '../../Assets/Images/facebook.svg';
 import twitterImg from '../../Assets/Images/twitter.svg';
 import webImg from '../../Assets/Images/web.svg';
+import { ImBin } from 'react-icons/im';
 
 const SocialCard = (props) => {
   let image = '';
@@ -19,17 +20,43 @@ const SocialCard = (props) => {
   else
     image = webImg;
 
+  const deleteSocial = () => {
+    const idx = props.counter;
+    const socials = props.profile.socials;
+
+    socials.splice(idx, 1);
+
+    props.setProfile({
+      ...props.profile,
+      socials: [...socials]
+    });
+  }
+
   return (
-    <div
-      className={props.overrideStyle || style.socialCard}
-      key={props.counter}
-      onClick={props.onClick}
-    >
-      {props.description ? <img src={image} alt="" className={style.socialIcons}/> : props.icon}
-      <div className={style.socialDesc}>
-        <p style={{fontWeight: "bold"}}>{props.title}</p>
-        <p style={{color: "#df691a", fontSize: "15px"}}>{props.description}</p>
+    <div className={style.socialCardWrap}>
+      <div
+        className={props.overrideStyle || style.socialCard}
+        key={props.counter}
+        onClick={props.onClick}
+      >
+        {props.description ? <img src={image} alt="" className={style.socialIcons}/> : props.icon}
+        <div className={style.socialDesc}>
+          <p style={{fontWeight: "bold"}}>{props.title}</p>
+          <p style={{color: "#df691a", fontSize: "15px"}}>{props.description}</p>
+        </div>
       </div>
+
+      {props.binIcon &&
+        props.allowInput ?
+          <ImBin
+            style={{color: "red"}}
+            size={"25px"}
+            onClick={deleteSocial}
+        /> : <ImBin
+          style={{ color: "gray" }}
+          size={"25px"}
+        />
+      }
     </div>
   );
 }
