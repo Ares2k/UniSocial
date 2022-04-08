d6a6a215846481ba1d75eab207990923a03f6c31
Timur Sultanov <Timursultanov00@gmail.com>
2022-04-08 19:20:58 +0100

    Refactored for easier readability

--- Server/Routes/users.js
+++ Server/Routes/users.js
@@ -6,24 +6,23 @@ const multer = require('multer');
 
 const upload = multer({ dest: 'uploads/' });
 
-router.post('/register', userController.register);
-router.post('/login'   , userController.login);
-router.post('/token'   , userController.generateNewToken);
-router.get('/hobbies'  , userController.getHobbies);
-router.post('/logout'          , checkAuth, userController.logout);
-router.post('/change-password' , checkAuth, userController.changePassword);
-router.get('/profile'          , checkAuth, userController.getProfile);
-router.put('/profile'          , checkAuth, userController.updateProfile);
-router.get('/mutual/'          , checkAuth, userController.mutualUsers);
-// router.get('/mutualUser/:id'   , checkAuth, userController.mutualUser);
-router.get('/mutualUser'       , checkAuth, userController.mutualUser);
-router.put('/images'           , checkAuth, upload.single('image'), userController.uploadImage);
-router.get('/images/:key'      , userController.getImage);
-router.get('/populate-hobbies' , userController.populateHobbies);
-router.post('/conversations'    , checkAuth, userController.conversations);
-router.get('/conversations/:userId', checkAuth, userController.getConversation);
-router.post('/messages', checkAuth, userController.sendMessage);
+//TODO: Separate routes
+
+router.get('/images/:key'                        , userController.getImage);
+router.post('/register'                          , userController.register);
+router.post('/login'                             , userController.login);
+router.post('/token'                             , userController.generateNewToken);
+router.get('/hobbies'                            , userController.getHobbies);
+router.get('/populate-hobbies'                   , userController.populateHobbies);
+router.post('/change-password'        , checkAuth, userController.changePassword);
+router.get('/profile'                 , checkAuth, userController.getProfile);
+router.put('/profile'                 , checkAuth, userController.updateProfile);
+router.get('/mutual/'                 , checkAuth, userController.mutualUsers);
+router.get('/mutualUser'              , checkAuth, userController.mutualUser);
+router.post('/conversations'          , checkAuth, userController.conversations);
+router.get('/conversations/:userId'   , checkAuth, userController.getConversation);
+router.post('/messages'               , checkAuth, userController.sendMessage);
 router.get('/messages/:conversationId', checkAuth, userController.getMessage);
+router.put('/images'                  , checkAuth, upload.single('image'), userController.uploadImage);
 
 module.exports = router;
-
