db60de6366cb9f3375eecb83085675c268c0aba3
Timur Sultanov <Timursultanov00@gmail.com>
2022-04-08 19:22:25 +0100

    socket server

---
+++ Socket/index.js
@@ -0,0 +1,47 @@
+const port = process.env.REACT_APP_SOCKET_PORT || 8900;
+const io = require('socket.io')(port, {
+  cors: {
+    origin: "http://localhost:3000"
+  }
+})
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  !users.some(user => user.userId === userId) &&
+    users.push({ userId, socketId });
+}
+
+const removeUser = (socketId) => {
+  users = users.filter(user => user.socketId !== socketId);
+}
+
+const getUser = (userId) => {
+  return users.find(user => user.userId === userId);
+}
+
+io.on("connection", (socket) => {
+  console.log("User connected");
+
+  socket.on("addUser", userId => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+
+    if (user?.socketId) {
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text
+      })
+    }
+  })
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected")
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  })
+})
